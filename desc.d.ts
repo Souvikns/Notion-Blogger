5ce6da5ae031a2ff0752cac7f89b545bd1785257
souvik <souvikde.ns@gmail.com>
2021-12-15 01:50:05 +0530

    feat: created type module declaration for notion-to-md

--- desc.d.ts
+++ desc.d.ts
@@ -1,1 +1,7 @@
-declare module "notion-to-md";
+declare module "notion-to-md" {
+    export default class Notion2md {
+        constructor({notionClient}: {notionClient: any});
+        pageToMarkdown(page_id: string): Array<any>;
+        toMarkdownString(mdBlocks: Array<any>): string;
+    }
+}
