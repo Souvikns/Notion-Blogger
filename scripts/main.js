f6094d836c441a1f8549898ba37b066b4138f299
Souvikns <souvikde.ns@gmail.com>
2022-01-02 21:51:22 +0530

    chore: remove some unnecessary files

--- scripts/main.js
+++
@@ -1,6 +0,0 @@
-require('dotenv').config;
-const NotionBlogger = require('../notion-blogger');
-
-
-NotionBlogger(process.env.NOTION_ACCESS_KEY, process.env.DATABASE_ID)
-    .catch(e => console.error(e));
